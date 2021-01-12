a40784122705073127f6cb1a7974ff4bd1a4e384
sojin.kim <sojin.kim@lemonhc.com>
2021-01-12 12:27:50 +0900

    README.md 수정

--- src/programmers/level1/Crain_64061.java
+++ src/programmers/level1/Crain_64061.java
@@ -1,7 +1,6 @@
 package programmers.level1;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+
 import java.util.Stack;
 
 public class Crain_64061 {
@@ -25,135 +24,102 @@ public class Crain_64061 {
 
 
         // 기대값 4
-        int result = solution(board, moves);
+        int result = solution2(board, moves);
         System.out.println(result);
 
     }
 
-    public static int solution(int[][] board, int[] moves) {
-        int answer = 0;
 
-        Stack<Integer> stack = new Stack<>();
+    public static int solution2(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> box = new Stack<>();
         for (int move : moves) {
-            for (int j=0; j<board.length; j++) {
-                if (board[j][move-1] != 0) {
-                    if (stack.isEmpty()) {
-                        stack.push(board[j][move-1]);
-                        board[j][move-1] = 0;
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][move - 1] != 0) { // 0이 아닐 때, 즉 빈 곳이 아닐 때
+                    if (box.isEmpty()) { // 박스가 비어있으면 인형을 넣음
+                        box.push(board[j][move - 1]);
+                        board[j][move - 1] = 0;
                         break;
                     }
-                    if(board[j][move-1] == stack.peek()) {
-                        stack.pop();
+                    if (board[j][move - 1] == box.peek()) { // 박스의 꼭대기와 같으면 없애버리고 result 2개 증가
+                        box.pop();
                         answer += 2;
+                    } else {
+                        box.push(board[j][move - 1]); // 박스의 꼭대기와 다르면 인형 넣기
 
                     }
+                    board[j][move - 1] = 0; // 인형 뽑았으면 없애기.
+                    break;
                 }
             }
         }
-
-
-
         return answer;
     }
-    public static int failedsolution(int[][] board, int[] moves) {
+
+    public static int failedsolution1(int[][] board, int[] moves) {
         int answer = 0;
 
         // 1 각 인형 힙에 넣어주기
-
-        Stack<Integer> crain1 = new Stack<Integer>();
-        for(int i=0; i<board[0].length; i++) {
-            if(board[0][i] != 0) {
-                crain1.push(board[0][i]);
-            }
-        }
-        Stack<Integer> crain2 = new Stack<Integer>();
-        for(int i=0; i<board[1].length; i++) {
-            if(board[1][i] != 0) {
-                crain2.push(board[1][i]);
-            }
-        }
-        Stack<Integer> crain3 = new Stack<Integer>();
-        for(int i=0; i<board[2].length; i++) {
-            if(board[2][i] != 0) {
-                crain3.push(board[2][i]);
-            }
-        }
-        Stack<Integer> crain4 = new Stack<Integer>();
-        for(int i=0; i<board[3].length; i++) {
-            if(board[3][i] != 0) {
-                crain4.push(board[3][i]);
-            }
-        }
-        Stack<Integer> crain5 = new Stack<Integer>();
-        for(int i=0; i<board[4].length; i++) {
-            if(board[4][i] != 0) {
-                crain5.push(board[4][i]);
-            }
-        }
+//        Stack<Integer> crain1 = new Stack<Integer>();
+//        for(int i=0; i<board[0].length; i++) {
+//            if(board[0][i] != 0) {
+//                crain1.push(board[0][i]);
+//            }
+//        }
+//        Stack<Integer> crain2 = new Stack<Integer>();
+//        for(int i=0; i<board[1].length; i++) {
+//            if(board[1][i] != 0) {
+//                crain2.push(board[1][i]);
+//            }
+//        }
+//        Stack<Integer> crain3 = new Stack<Integer>();
+//        for(int i=0; i<board[2].length; i++) {
+//            if(board[2][i] != 0) {
+//                crain3.push(board[2][i]);
+//            }
+//        }
+//        Stack<Integer> crain4 = new Stack<Integer>();
+//        for(int i=0; i<board[3].length; i++) {
+//            if(board[3][i] != 0) {
+//                crain4.push(board[3][i]);
+//            }
+//        }
+//        Stack<Integer> crain5 = new Stack<Integer>();
+//        for(int i=0; i<board[4].length; i++) {
+//            if(board[4][i] != 0) {
+//                crain5.push(board[4][i]);
+//            }
+//        }
         // 이까진 완료
 
 
         // 2.바구니에 인형쌓기 위해 인형 집기
         // 2-2 인형 넣는 바구니 배열 선언
-        ArrayList<Integer> box = new ArrayList<>();
-        int preNum = 0;
-        // 2-3 인형 집기 각 줄에서 마지막꺼
-        for(int i=0; i<moves.length; i++) {
-            if (preNum != moves[i]) {
-                System.out.println(preNum);
-                System.out.println(box.toString());
-                switch(moves[i]) {
-                    case 1:
-                        if (!crain1.empty()) {
-                            int s = crain1.pop();
-                            box.add(s);
-                            preNum = s;
-                        }
+
+        Stack<Integer> box = new Stack<>();
+        for (int i : moves) {
+            for (int j=0; j<board.length; j++) { //board.length = 5
+                if (board[j][i-1] != 0) {
+//                    System.out.println("j:"+j+" i:"+i+" board[j][i-1]:"+board[j][i-1]);
+                    if (box.empty()) { // 박스 비었을 때
+                        box.push(board[j][i-1]);
                         break;
-                    case 2:
-                        if (!crain2.empty()) {
-                            int s = crain2.pop();
-                            box.add(s);
-                            preNum = s;
+                    } else {
+                        if (box.peek() == board[j][i-1]) { // 인형 터뜨리기
+                            box.pop();
+                            answer += 2;
+                        } else {
+                            box.push(board[j][i-1]); // 인형 넣기
                         }
-                        break;
-                    case 3:
-                        if (!crain3.empty()) {
-                            int s = crain3.pop();
-                            box.add(s);
-                            preNum = s;
-                        }
-                        break;
-                    case 4:
-                        if (!crain4.empty()) {
-                            int s = crain4.pop();
-                            box.add(s);
-                            preNum = s;
-                        }
-                        break;
-                    case 5:
-                        if (!crain5.empty()) {
-                            int s = crain5.pop();
-                            box.add(s);
-                            preNum = s;
-                        }
-                        break;
+                    }
+                    board[j][i-1] = 0 ; // 뽑은 인형 비우기
+                    break; // 빠져나옴
                 }
-            }
-        }
-        System.out.println(box.toString());
 
-        // 3. 마지막 중복 제거해주기
-        for (int i=1; i<box.size(); i++) {
-            if( box.get(i-1) == box.get(i) ) {
-                box.remove(i-1);
-                System.out.println(box.toString());
             }
         }
 
-        answer = box.size();
-
-    return answer;
+        return answer;
     }
 
 
