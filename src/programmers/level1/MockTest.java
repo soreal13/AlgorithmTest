fe50191c9cfa28c3840a49e33673a05558b750d1
sojin.kim <sojin.kim@lemonhc.com>
2021-01-14 13:54:11 +0900

    (세팅) lv1 모의고사

---
+++ src/programmers/level1/MockTest.java
@@ -0,0 +1,24 @@
+package programmers.level1;
+
+public class MockTest {
+
+    public static void main(String[] args) {
+
+        int[] answerTest1 = {1,2,3,4,5};
+        int[] answerTest2 = {1,2,3};
+
+        int[] answer = solution(answerTest1);
+
+        for(int i=0; i<answerTest1.length; i++) {
+            System.out.println(answerTest1[i]);
+        }
+
+    }
+
+    public static int[] solution(int[] answers) {
+        int[] answer = {};
+
+        return answer;
+    }
+
+}
