76ccf5b3c1093110cd8dccb70b162dd5a98216d2
sojin.kim <sojin.kim@lemonhc.com>
2021-01-18 17:56:31 +0900

    선택정렬

---
+++ src/think_data_sturctures/SelectionSort.java
@@ -0,0 +1,41 @@
+package think_data_sturctures;
+
+public class SelectionSort {
+
+    // 선택 정렬
+
+    /**
+     * i와 j의 위치에 있는 값을 바꿈
+     */
+    public static void swapElements(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    /**
+     * start로부터 시작하는 최솟값의 위치를 찾고(start 포함)
+     * 배열의 마지막 위치로 감
+     */
+    public static int indexLowest(int[] array, int start) {
+        int lowIndex = start;
+        for (int i=start; i<array.length; i++) {
+            if (array[i] < array[lowIndex]) {
+                lowIndex = i;
+            }
+        }
+        return lowIndex;
+    }
+
+    /**
+     * 선택 정렬 사용하여 요소 정렬
+     */
+    public static void selectionSort(int[] array) {
+        for (int i=0; i<array.length; i++) {
+            int j = indexLowest(array, i);
+            swapElements(array, i , j);
+        }
+    }
+
+
+}
