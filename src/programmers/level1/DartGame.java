ed5b255a61c5f38937d261b98fc91b02f2af9fc2
sojin.kim <sojin.kim@lemonhc.com>
2021-01-19 16:12:38 +0900

    (세팅) lv1 다트게임

---
+++ src/programmers/level1/DartGame.java
@@ -0,0 +1,35 @@
+package programmers.level1;
+
+public class DartGame {
+
+//    0~10의 정수와 문자 S, D, T, *, #로 구성된 문자열이 입력될 시
+//    총점수를 반환하는 함수를 작성하라.
+//
+//            입력 형식
+//    점수|보너스|[옵션]으로 이루어진 문자열 3세트.
+//            예) 1S2D*3T
+//
+//    점수는 0에서 10 사이의 정수이다.
+//    보너스는 S, D, T 중 하나이다.
+//    옵선은 *이나 # 중 하나이며, 없을 수도 있다.
+
+
+    public static void main(String[] args) {
+
+        String dartTest1 = "1S2D*3T";
+        String dartTest2 = "1D2S#10S";
+        String dartTest3 = "1D2S0T";
+
+
+    }
+
+
+    public int solution(String dartResult) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+}
