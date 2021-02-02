fb69eae47162ff3fa8e66dffb2d68c6a0884caa3
sojin.kim <sojin.kim@lemonhc.com>
2021-02-02 09:34:11 +0900

    (세팅) 프로그래머스 Lv1 문제

---
+++ src/programmers/level1/skilltest1.java
@@ -0,0 +1,28 @@
+package programmers.level1;
+
+public class skilltest1 {
+
+    public static void main(String[] args) {
+
+        String s1 = "AB";
+        String s2 = "a";
+        String s3 = "a B z";
+
+        String answer1 = solution(s1, 1);
+        String answer2 = solution(s2 ,1);
+        String answer3 = solution(s3, 4);
+
+        System.out.println(answer1);
+        System.out.println(answer2);
+        System.out.println(answer3);
+    }
+
+    public static String solution(String s, int n) {
+        String answer = "";
+
+
+
+        return answer;
+    }
+
+}
