8fa5deda1f12f54abf7550eb67855fbbe5d3ddda
sojin.kim <sojin.kim@lemonhc.com>
2021-02-17 12:11:28 +0900

    (풀이) 자바 스킬셋 탐욕법

---
+++ src/programmers/skillset/wireless_statation.java
@@ -0,0 +1,80 @@
+package programmers.skillset;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class wireless_statation {
+
+
+    public static void main(String[] args) {
+        int n1 = 11;
+        int[]stations1 = {4,11};
+        int w1 = 1;
+        int answer1 = 3;
+
+        int n2 = 16;
+        int[]station2 = {9};
+        int w2 = 2;
+        int answer2 = 3;
+
+        int realAnswer = solution(n1, stations1, w1);
+        int realAnswer2 = solution(n2, station2, w2);
+
+        System.out.println(realAnswer);
+        System.out.println(realAnswer2);
+
+    }
+
+
+    public static int solution(int n, int[] stations, int w) {
+        int answer = 0;
+
+        // greedy 탐욕법
+        Queue<Integer> sq = new LinkedList<>();
+        for(int s: stations) sq.offer(s);
+
+        int position = 1;
+        while(position <= n) {
+            if(!sq.isEmpty() && sq.peek() - w <= position ) {
+                position = sq.poll() + w + 1;
+            } else {
+                answer += 1; // 기지국 하나 설치
+                position += w*2 + 1; //기지국 범위만큼 이동
+            }
+        }
+
+        return answer;
+    }
+
+    public static int solution2(int n, int[] stations, int w) {
+        int answer = 0;
+
+        // Object보다 원시형
+        // 루프문 의
+
+        int si=0;
+        int position = 1;
+        while(position <= n) {
+            if(si < stations.length && stations[si] - w <= position ) {
+                position = stations[si] + w + 1;
+            } else {
+                answer += 1; // 기지국 하나 설치
+                position += w*2 + 1; //기지국 범위만큼 이동
+            }
+        }
+
+        return answer;
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
