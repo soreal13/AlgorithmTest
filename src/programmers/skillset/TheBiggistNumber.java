450510b5844b24026e8269ecc06f13f5414d5476
sojin.kim <sojin.kim@lemonhc.com>
2021-02-17 14:53:06 +0900

    가장 큰 수 문제 세팅

---
+++ src/programmers/skillset/TheBiggistNumber.java
@@ -0,0 +1,28 @@
+package programmers.skillset;
+
+public class TheBiggistNumber {
+
+
+    public static void main(String[] args) {
+
+        int[] numbers1 = {6, 10, 2};
+        int[] numbers2 = {3, 30, 34, 5, 9};
+
+        TheBiggistNumber tbn = new TheBiggistNumber();
+
+        System.out.println(tbn.solution(numbers1));
+        System.out.println(tbn.solution(numbers2));
+
+    }
+
+    public String solution(int[] numbers) {
+        String answer = "";
+        return answer;
+    }
+
+    public String solution2(int[] numbers) {
+        String answer = "";
+        return answer;
+    }
+
+}
